ac5a5740deea6f557daa0a2b752f4766ee318cc5
kangsiwoo <bssmkangsiwoo@gmail.com>
2023-07-13 00:19:01 +0900

    DELETE :: overlapping photos

--- js/change-color.js
+++ js/change-color.js
@@ -1,10 +1,24 @@
+var colorNmae = "deep_purple"
 var nowColor = "DP"
 function changeColor(color) {
+    if (color === "DP") {
+        colorName = "deep_purple"
+    } else if (color === "G") {
+        colorName = "gold"
+    } else if (color === "S") {
+        colorNmae = "silver"
+    } else {
+        colorNmae = "space_black"
+    }
+
+
+
     if (changeAllow === true) {
         if (nowColor !== color) {
             document.getElementById("iphone" + nowColor).style.opacity = 0
             setTimeout(function() {
                 document.getElementById("iphone" + color).style.opacity = 1
+                document.getElementById("DIPicture").src = "./source/dynamic_island_" + colorNmae + "__exowosw6732a_large.jpg"
             }, 1000); // 1000밀리초 = 1초
 
             nowColor = color
