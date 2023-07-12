f37726a99e1d70865df1ed43efd641901c251a9a
kangsiwoo <bssmkangsiwoo@gmail.com>
2023-07-13 00:09:26 +0900

    DELETE :: First change

--- js/change-color.js
+++ js/change-color.js
@@ -1,33 +1,14 @@
-var firstchange = true
 var nowColor = "DP"
 function changeColor(color) {
-    if (firstchange === false) {
-        if (changeAllow === true) {
-            if (nowColor !== color) {
-                document.getElementById("iphone" + nowColor).style.opacity = 0
-                setTimeout(function() {
-                    document.getElementById("iphone" + color).style.opacity = 1
-                }, 1000); // 1000밀리초 = 1초
+    if (changeAllow === true) {
+        if (nowColor !== color) {
+            document.getElementById("iphone" + nowColor).style.opacity = 0
+            setTimeout(function() {
+                document.getElementById("iphone" + color).style.opacity = 1
+            }, 1000); // 1000밀리초 = 1초
 
-                nowColor = color
-            }
+            nowColor = color
         }
     }
-    else {
-        setTimeout(function () {
-            if (changeAllow === true) {
-                if (nowColor !== color) {
-                    document.getElementById("iphone" + nowColor).style.opacity = 0
-                    setTimeout(function() {
-                        document.getElementById("iphone" + color).style.opacity = 1
-                    }, 1000); // 1000밀리초 = 1초
-
-                    nowColor = color
-                    firstchange = false
-                }
-            }
-        }, 1000)
-    }
-
 
 }
