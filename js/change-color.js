11ec63b047c029af4a7c8fa7bd6b07f8b4493ad0
kangsiwoo <bssmkangsiwoo@gmail.com>
2023-07-13 01:02:22 +0900

    ADD :: Info color change

--- js/change-color.js
+++ js/change-color.js
@@ -1,24 +1,29 @@
-var colorNmae = "deep_purple"
 var nowColor = "DP"
 function changeColor(color) {
-    if (color === "DP") {
-        colorName = "deep_purple"
-    } else if (color === "G") {
-        colorName = "gold"
-    } else if (color === "S") {
-        colorNmae = "silver"
-    } else {
-        colorNmae = "space_black"
-    }
-
-
-
     if (changeAllow === true) {
         if (nowColor !== color) {
             document.getElementById("iphone" + nowColor).style.opacity = 0
             setTimeout(function() {
                 document.getElementById("iphone" + color).style.opacity = 1
-                document.getElementById("DIPicture").src = "./source/dynamic_island_" + colorNmae + "__exowosw6732a_large.jpg"
+                document.getElementById("DIPicture").src = "./source/DI-" + color + ".jpg"
+                document.getElementById("A16").style.backgroundImage = 'url(".././source/chip-' + color + '.jpg")'
+                if (color === "DP") {
+                    document.querySelectorAll(".textGradient").forEach(element => {
+                        element.style.backgroundImage = 'linear-gradient(180deg,#ffb6ff,#b344ff)';
+                    });
+                } else if (color === "G") {
+                    document.querySelectorAll(".textGradient").forEach(element => {
+                        element.style.backgroundImage = 'linear-gradient(180deg,#ffe993,#eb831c)';
+                    });
+                } else if (color === "S") {
+                    document.querySelectorAll(".textGradient").forEach(element => {
+                        element.style.backgroundImage = 'linear-gradient(180deg,#c6deff,#516cec)';
+                    });
+                } else {
+                    document.querySelectorAll(".textGradient").forEach(element => {
+                        element.style.backgroundImage = 'linear-gradient(180deg,#dbcedb,#60657f)';
+                    });
+                }
             }, 1000); // 1000밀리초 = 1초
 
             nowColor = color
