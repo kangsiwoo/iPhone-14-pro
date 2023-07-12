75190eee3e552e5b3f598e94e208ede90b32c467
kangsiwoo <bssmkangsiwoo@gmail.com>
2023-07-13 03:02:32 +0900

    EDIT :: follower details

--- js/follower.js
+++ js/follower.js
@@ -1,24 +1,24 @@
 var follower = document.getElementById("follower");
 var scrollTop = document.documentElement.scrollTop;
 var oldScroll = document.documentElement.scrollTop;
-var follow = true
+var follow = true;
 
 window.addEventListener('scroll', function() {
     scrollTop = document.documentElement.scrollTop;
-    console.log(scrollTop)
-    if (scrollTop > 145) {
-        follow = false
-        follower.style.backgroundColor = "rgba(0, 0, 0, 0.5)"
+    console.log(scrollTop);
+
+    if (scrollTop > 131) {
+        follow = false;
+        follower.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+        follower.style.top = "0";
     } else {
-        follow = true
-        follower.style.backgroundColor = "rgba(0, 0, 0, 0)"
+        follow = true;
+        follower.style.backgroundColor = "rgba(0, 0, 0, 0)";
     }
+
     if (follow) {
-
-        var translateY = parseInt(getComputedStyle(follower).transform.split(',')[5]) || 0;
-        follower.style.transform = `translateY(${translateY - (scrollTop - oldScroll)}px)`;
+        var topValue = parseInt(follower.style.top) || 0;
+        follower.style.top = `${topValue - (scrollTop - oldScroll)}px`;
         oldScroll = scrollTop;
-    } else {
-
     }
 });
